15d89e1675375a0d105adb3e516e132b5768d639
Joshua Toye <josh.tom15@gmail.com>
2019-09-24 17:16:22 -0500

    Updated so that damage is counted for every block broken, not just every tree.

--- src/main/java/me/joshuatoye/treeFeller/BlockBreakListener.java
+++ src/main/java/me/joshuatoye/treeFeller/BlockBreakListener.java
@@ -2,16 +2,23 @@ package me.joshuatoye.treeFeller;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerItemBreakEvent;
+import org.bukkit.event.server.BroadcastMessageEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.EnumSet;
 import java.util.Set;
 import java.util.Stack;
 
 public class BlockBreakListener implements Listener {
+
+    private static final int GOLDEN_AXE_DURABILITY = 32;
 
     private static final Set<Material> LOGS = EnumSet.of(
            Material.BIRCH_LOG, Material.ACACIA_LOG, Material.DARK_OAK_LOG,
@@ -20,38 +27,49 @@ public class BlockBreakListener implements Listener {
 
     private Stack<Block> breakingTree = new Stack<Block>();
 
-    private void breakTree(Block breakingBlock)
+    private int breakTree(Block breakingBlock, int damageDone)
     {
         breakingTree.push(breakingBlock);
         Block tempBlock = breakingBlock.getRelative(1,0,0);
 
         for(int i = -1; i < 2; i++)
             for(int j = -1; j < 2; j++)
-                for(int k = -1; k < 2; k++)
-                {
+                for(int k = -1; k < 2; k++) {
                     tempBlock = breakingBlock.getRelative(i, j, k);
-                    if(LOGS.contains(tempBlock.getType()) && breakingTree.search(tempBlock) == -1)
-                    {
-                        breakTree(tempBlock);
+                    if (LOGS.contains(tempBlock.getType()) && breakingTree.search(tempBlock) == -1) {
+                        damageDone += breakTree(tempBlock, damageDone);
                     }
                 }
 
         breakingTree.pop();
         breakingBlock.breakNaturally();
+        damageDone++;
+        return damageDone;
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event)
     {
         ItemStack mainHand = event.getPlayer().getInventory().getItemInMainHand();
-
         //MainHand mainHand = event.getPlayer().getMainHand();
         Block block = event.getBlock();
         //mainHand.
 
         if(LOGS.contains(block.getType()) && mainHand.getType() == Material.GOLDEN_AXE)
         {
-            breakTree(block);
+            int damage = 0;
+            damage = breakTree(block, damage);
+
+            ItemMeta axeMeta = mainHand.getItemMeta();
+            int damageDoneAlready = ((Damageable)axeMeta).getDamage();
+            System.out.println(damage + damageDoneAlready);
+            ((Damageable)axeMeta).setDamage(damageDoneAlready + damage);
+            mainHand.setItemMeta(axeMeta);
+
+            if(((Damageable) mainHand.getItemMeta()).getDamage() >= GOLDEN_AXE_DURABILITY)
+            {
+                mainHand.setAmount(0);
+            }
         }
     }
 
